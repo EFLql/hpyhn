524c2cf095411aa5011e9e6e324b4bbfeb5acc4e
EFLql <1633121980@qq.com>
2025-10-04 16:46:01 +0800

    [fix]sitemap update frequency change to hourly

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,23 +3,29 @@ export default function sitemap() {
     {
       url: 'https://hpyhn.xyz',
       lastModified: new Date(),
-      changeFrequency: 'daily',
+      changeFrequency: 'hourly',
       priority: 1,
     },
     {
       url: 'https://hpyhn.xyz/ask',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'hourly',
       priority: 0.8,
     },
     {
       url: 'https://hpyhn.xyz/newest',
       lastModified: new Date(),
-      changeFrequency: 'daily',
+      changeFrequency: 'hourly',
       priority: 0.9,
     },
     {
       url: 'https://hpyhn.xyz/show',
+      lastModified: new Date(),
+      changeFrequency: 'hourly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://hpyhn.xyz/weekly',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.9,
